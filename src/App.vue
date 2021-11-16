f0cbd0013f8880b9b7de64a579b09ac55d11134f
liuzixuan <76858586@qq.com>
2021-11-16 16:54:06 +0800

    render函数h函数与vue3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
+<!--
+ * @Descripttion: 纵有千古，横有八荒，前途似海，来日方长。
+ * @version: 1.0
+ * @Author: 番茄
+ * @Date: 2021-11-12 15:35:18
+ * @LastEditors: 番茄
+ * @LastEditTime: 2021-11-16 16:44:04
+-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  {{ name }}
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, reactive, toRefs, watchEffect } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld,
   },
-});
+  setup() {
+    const state = reactive({
+      name: 'fanqie',
+    })
+    setInterval(() => {
+      state.name += 1
+    }, 1000)
+
+    const data = toRefs(state)
+    return {
+      ...data,
+    }
+  },
+})
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
+/*
+ * @Descripttion: 纵有千古，横有八荒，前途似海，来日方长。
+ * @version: 1.0
+ * @Author: 番茄
+ * @Date: 2021-11-12 15:35:18
+ * @LastEditors: 番茄
+ * @LastEditTime: 2021-11-16 15:51:05
+ */
+import { createApp, defineComponent, h } from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
+// h函数是对createVNode的进一步封装使用
+// const App = defineComponent({
+//   render() {
+//     return h('div', { id: 'app' }, [
+//       h('img', {
+//         src: '',
+//       }),
+//       h(HelloWorld),
+//     ])
+//   },
+// })
 createApp(App).mount('#app')
