5b75ed57f4f3cec50fdab24e0b429264e944f0eb
liuzixuan <76858586@qq.com>
2021-11-17 17:26:29 +0800

    11

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@
  * @Author: 番茄
  * @Date: 2021-11-12 15:35:18
  * @LastEditors: 番茄
- * @LastEditTime: 2021-11-16 15:51:05
+ * @LastEditTime: 2021-11-17 09:43:22
  */
-import { createApp, defineComponent, h } from 'vue'
-import App from './App.vue'
+import { createApp, defineComponent, h, reactive } from 'vue'
+// import App from './App.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 // h函数是对createVNode的进一步封装使用
@@ -21,4 +21,21 @@ import HelloWorld from './components/HelloWorld.vue'
 //     ])
 //   },
 // })
+
+const App = defineComponent({
+  setup() {
+    const state = reactive({
+      name: 'fanqie',
+    })
+
+    return () => {
+      return h('div', { id: 'app' }, [
+        h('img', {
+          src: '',
+        }),
+        h('p', state.name),
+      ])
+    }
+  },
+})
 createApp(App).mount('#app')
